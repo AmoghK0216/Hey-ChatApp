a35a598ca682b457ee3cd9af5eab8ae8b365133e
Amogh Krishna Padakanti <amoghkrishna16@gmail.com>
2026-03-10 15:06:31 -0400

    added rabbitMQ consumer

---
+++ backend/mail/src/consumer.ts
@@ -0,0 +1,52 @@
+import amqp from "amqplib";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const startSendOtpConsumer = async () => {
+  try {
+    const connection = await amqp.connect({
+      protocol: "amqp",
+      hostname: process.env.RABBITMQ_HOSTNAME,
+      port: 5672,
+      username: process.env.RABBITMQ_USERNAME,
+      password: process.env.RABBITMQ_PASSWORD,
+    });
+
+    const channel = await connection.createChannel();
+    const queueName = "send-otp";
+    channel.assertQueue(queueName, { durable: true });
+
+    channel.consume(queueName, async (msg) => {
+      if (msg) {
+        try {
+          const { to, subject, body } = JSON.parse(msg.content.toString());
+
+          const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            auth: {
+              user: process.env.USER,
+              pass: process.env.PASSWORD,
+            },
+          });
+
+          await transporter.sendMail({
+            from: "Hey-ChatApp",
+            to,
+            subject,
+            text: body,
+          });
+
+          console.log(`OTP sent to ${to}`);
+          channel.ack(msg);
+        } catch (error) {
+          console.log("Failed to send otp", error);
+        }
+      }
+    });
+  } catch (error) {
+    console.log("Failed to start to rabbitMQ consumer", error);
+  }
+};
