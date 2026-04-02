18911975d9294617bfad2c3a9686eb5a3b145fa7
AmoghK0216 <75850896+AmoghK0216@users.noreply.github.com>
2026-04-02 17:05:18 -0400

    user typing indicator in UI (#15)

--- backend/chat/src/config/socket.ts
+++ backend/chat/src/config/socket.ts
@@ -35,6 +35,36 @@ io.on("connection", (socket: Socket) => {
     }
   });
 
+  if (userId) {
+    socket.join(userId);
+  }
+
+  socket.on("typing", (data) => {
+    console.log(`User ${data.userId} is typing in Chat ${data.chatId}`);
+    socket.to(data.chatId).emit("userTyping", {
+      chatId: data.chatId,
+      userId: data.userId,
+    });
+  });
+
+  socket.on("stopTyping", (data) => {
+    console.log(`User ${data.userId} stopped typing in chat ${data.chatId}`);
+    socket.to(data.chatId).emit("userStoppedTyping", {
+      chatId: data.chatId,
+      userId: data.userId,
+    });
+  });
+
+  socket.on("joinChat", (chatId) => {
+    socket.join(chatId);
+    console.log(`User ${userId} joined chat room ${chatId}`);
+  });
+
+  socket.on("leaveChat", (chatId) => {
+    socket.leave(chatId);
+    console.log(`User ${userId} left chat room ${chatId}`);
+  });
+
   socket.on("connect_error", (error) => {
     console.log("Socket Connection Error", error);
   });
